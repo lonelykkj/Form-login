6a762b114720e84b994bcd1860786a96b54bd7c9
Heitor <heitormanoel05@gmail.com>
2023-10-03 16:49:56 -0300

    fix config.js

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8085;
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('formLogin', 'root', '!h123456', {
-  host: "localhost",
-  dialect: "mysql"
-})
-
-sequelize.authenticate().then(function () {
-  console.log('Conectado com sucesso!')
-}).catch(function (erro) {
-  console.log("Fala ao se conectar: "+erro)
-})
+require("./config")
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
---
+++ config.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('formLogin', 'root', '!h123456', {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+sequelize.authenticate().then(function () {
+    console.log('Conectado com sucesso!')
+  }).catch(function (erro) {
+    console.log("Fala ao se conectar: "+erro)
+  })
+  
+module.exports = sequelize
